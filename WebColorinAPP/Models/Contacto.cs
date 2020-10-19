72a53af1ea53d7d7256f9687566ef4f3906dc4cd
dilacio <dilacio@hotmail.com>
2020-10-19 12:39:51 -0300

    Version inicial del proyecto

---
+++ WebColorinAPP/Models/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebColorinAPP.Models
+{
+    public class Contacto
+    {
+        public String Nombre { get; set; }
+        public String Mail { get; set; }
+        public int Telefono { get; set; }
+        public String Mensaje { get; set; }
+
+        public Contacto(string nombre, string mail, int telefono, string mensaje)
+        {
+            Nombre = nombre;
+            Mail = mail;
+            Telefono = telefono;
+            Mensaje = mensaje;
+        }
+
+        public Contacto()
+        {
+        }
+    }
+}
---
+++ WebColorinAPP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebColorinAPP.Startup))]
+namespace WebColorinAPP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
